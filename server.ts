b56cd6f8c6173479ea4096efa7214c9dd4614976
hanafi <hanafi.abderrahmen@appsintegra.com>
2020-05-30 15:19:12 +0400

    read swagger from controller

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 // this file is for development purpose and will not be published to npm
 import * as express from 'express';
+import * as swaggerValidation from './lib';
 
 const app = express();
 const PORT = 2900;
+swaggerValidation;
 app.listen(PORT);
 console.log('_____________/\\_________________');
 console.log(`  server running at port ${PORT}`);
--- src/core/readSwagger.ts
+++ src/core/readSwagger.ts
@@ -4,10 +4,8 @@ const readSwagger = (path: string): Swagger | Error => {
     let swagger: Swagger;
     try {
         swagger = JSON.parse(fs.readFileSync(path, { encoding: 'utf8' }));
-        console.log(swagger);
         return swagger;
     } catch (error) {
-        console.log(error.message);
         return new Error('Error reading swagger file');
     }
 };
